dc1def5b0eb390290f07b36eb9168b295b404bfa
Tony Deng <wolf.deng@gmail.com>
2018-09-30 15:00:25 +0800

    :white_check_mark: test(1.0): 添加基于junit-5.3.1的测试
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ samples-facade/src/main/java/com/github/tonydeng/demo/grpc/facade/Calculator.java
@@ -0,0 +1,8 @@
+package com.github.tonydeng.demo.grpc.facade;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ samples-facade/src/test/java/com/github/tonydeng/demo/grpc/facade/CalculatorTests.java
@@ -0,0 +1,33 @@
+package com.github.tonydeng.demo.grpc.facade;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTests {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addTwoNumbers() {
+		Calculator calculator = new Calculator();
+
+		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+	}
+
+	@ParameterizedTest(name = "{0} + {1} = {2}")
+	@CsvSource({
+			"0, 1,  1",
+			"1, 2,  3",
+			"49,    51, 100",
+			"1, 100,    101"
+	})
+	void add(int first, int second, int exectedResult) {
+		Calculator calculator = new Calculator();
+
+		assertEquals(exectedResult, calculator.add(first, second),
+				() -> first + " + " + second + " should equal " + exectedResult);
+	}
+}
