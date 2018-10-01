2950506b2072943de875ee93fe645d9c5bcab361
Tony Deng <wolf.deng@gmail.com>
2018-10-01 17:31:17 +0800

    :sparkles: feat(1.0): 添加Thrift基于Springboot启动服务
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ thrift-samples/thrift-samples-producer/src/main/java/com/github/tonydeng/demo/rpc/thrift/producer/Application.java
@@ -0,0 +1,36 @@
+package com.github.tonydeng.demo.rpc.thrift.producer;
+
+import com.github.tonydeng.demo.rpc.thrift.BookService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TTransportException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.github.tonydeng.demo.rpc.thrift"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+
+        try {
+            log.info("Thrift RPC's Server Start......");
+            TProcessor processor = new BookService.Processor<BookService.Iface>(new BookServiceProducer());
+            TServerSocket serverTransport = new TServerSocket(9000);
+
+            TServer.Args tArgs = new TServer.Args(serverTransport);
+            tArgs.processor(processor);
+            tArgs.protocolFactory(new TBinaryProtocol.Factory());
+            TServer server = new TSimpleServer(tArgs);
+            server.serve();
+        } catch (TTransportException e) {
+            log.error("Thrift RPC's Server serve error \n", e);
+        }
+    }
+}
--- thrift-samples/thrift-samples-producer/src/main/java/com/github/tonydeng/demo/rpc/thrift/producer/BookServiceProducer.java
+++ thrift-samples/thrift-samples-producer/src/main/java/com/github/tonydeng/demo/rpc/thrift/producer/BookServiceProducer.java
@@ -4,13 +4,11 @@ import com.github.tonydeng.demo.rpc.thrift.Book;
 import com.github.tonydeng.demo.rpc.thrift.BookService.Iface;
 import com.github.tonydeng.demo.rpc.utils.BookUtils;
 import org.apache.thrift.TException;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@SpringBootConfiguration
 @Service("BookService")
 public class BookServiceProducer implements Iface {
     @Override
