4b7bb55a6130bdde030494ff907dccb496d400f8
Tony Deng <wolf.deng@gmail.com>
2018-10-01 22:28:43 +0800

    :white_check_mark: test(1.0): 添加Thrift转json的测试
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- thrift-samples/thrift-samples-facade/src/test/java/com/github/tonydeng/demo/rpc/thrift/facade/BookTest.java
+++ thrift-samples/thrift-samples-facade/src/test/java/com/github/tonydeng/demo/rpc/thrift/facade/BookTest.java
@@ -1,4 +1,33 @@
 package com.github.tonydeng.demo.rpc.thrift.facade;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.github.tonydeng.demo.rpc.thrift.Book;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@Slf4j
 public class BookTest {
+    @CsvSource({"foo, 1", "bar, 2", "'baz, qux', 3"})
+    @ParameterizedTest
+    public void writeToJson(String title, int page) {
+        log.info("{}, {}", title, page);
+
+        Book book = new Book().setTitle(title).setPage(page);
+
+        assertEquals(title, book.getTitle());
+        assertEquals(page, book.getPage());
+
+        JSONObject jo = JSON.parseObject(JSON.toJSONString(book));
+
+        assertEquals(book.getTitle(),jo.getString("title"));
+        assertEquals(book.getPage(),jo.getIntValue("page"));
+        assertNull(book.getISBN());
+        assertNull(jo.get("ISBN"));
+    }
+
 }
