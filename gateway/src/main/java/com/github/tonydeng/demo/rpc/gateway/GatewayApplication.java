d8e8a43dc3e351bcae8afe89500590c01bffc97f
Tony Deng <wolf.deng@gmail.com>
2018-10-02 01:08:33 +0800

    :hammer: refactor(1.0): 添加Resources的单元测试
    refs #1
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- gateway/src/main/java/com/github/tonydeng/demo/rpc/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/github/tonydeng/demo/rpc/gateway/GatewayApplication.java
@@ -3,11 +3,12 @@ package com.github.tonydeng.demo.rpc.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.ws.rs.core.Application;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"com.github.tonydeng.demo.rpc.*"})
-public class GatewayApplication extends SpringBootServletInitializer {
+public class GatewayApplication extends Application {
 
     public static void main(String[] args) {
         log.info("Gateway Application start......");
