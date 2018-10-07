e8b536a89edc23cf408d3b753413718ecfc8c569
Tony Deng <wolf.deng@gmail.com>
2018-10-07 23:39:35 +0800

    :white_check_mark: test(1.0): 添加grpc BookServiceProducer的初始化数据测试
    refs #3
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- grpc-samples/grpc-samples-producer/src/test/java/com/github/tonydeng/demo/grpc/producer/BookServiceProduecrTest.java
+++ grpc-samples/grpc-samples-producer/src/test/java/com/github/tonydeng/demo/grpc/producer/BookServiceProduecrTest.java
@@ -1,13 +1,20 @@
 package com.github.tonydeng.demo.grpc.producer;
 
+import com.github.tonydeng.demo.rpc.proto.Book;
+import com.github.tonydeng.demo.rpc.proto.BookList;
 import com.github.tonydeng.demo.rpc.proto.BookServiceGrpc;
+import com.github.tonydeng.demo.rpc.proto.BookType;
+import com.google.common.collect.Lists;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -19,8 +26,42 @@ public class BookServiceProduecrTest {
     @Resource
     private BookServiceGrpc.BookServiceImplBase grpcBookService;
 
+    private static BookList bookList;
+
+    private static StreamObserver<BookList> response;
+
+    @BeforeAll
+    public static void setUp() {
+        bookList = BookList.getDefaultInstance();
+
+        bookList.toBuilder().addAllBook(Lists.newArrayList(
+                Book.newBuilder()
+                        .setAuthor("A1")
+                        .setPage(1)
+                        .setBookType(BookType.BOOK)
+                        .setTitle("T1")
+                        .addAllKeyword(Arrays.asList("k", "e", "y"))
+                        .build(),
+                Book.newBuilder()
+                        .setAuthor("A2")
+                        .setPage(2)
+                        .setBookType(BookType.BOOK)
+                        .setTitle("T2")
+                        .addAllKeyword(Arrays.asList("k", "e", "y"))
+                        .build()
+        ));
+
+    }
+
     @Test
     void testSpringBoot() {
         assertNotNull(grpcBookService);
+    }
+
+    @Test
+    void createBooksTest() {
+        grpcBookService.createBooks(bookList, response);
+        assertNotNull(bookList);
+        assertNotNull(response);
     }
 }
