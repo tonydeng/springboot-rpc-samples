cffcca86dda879c323134072ee60bcc2cf913b58
Tony Deng <wolf.deng@gmail.com>
2018-10-10 01:51:52 +0800

    :sparkles: feat(1.0): 定义结构体和接口
    refs #7
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ armeria-samples/armeria-samples-facade/src/test/java/com/github/tonydeng/demo/rpc/armeria/facade/BaseTest.java
@@ -0,0 +1,13 @@
+package com.github.tonydeng.demo.rpc.armeria.facade;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class BaseTest {
+
+    @Test
+    void test() {
+        log.info("armeria facade test......");
+    }
+}
