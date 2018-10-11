54c271ab30e82b334efe8fcf2231e7eb4bf654d1
Tony Deng <wolf.deng@gmail.com>
2018-10-11 12:29:19 +0800

    :sparkles: feat(1.0): 添加端口 是否被占用的验证方法
    refs #7
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- utils/src/main/java/com/github/tonydeng/demo/rpc/utils/NetworkUtils.java
+++ utils/src/main/java/com/github/tonydeng/demo/rpc/utils/NetworkUtils.java
@@ -1,5 +1,6 @@
 package com.github.tonydeng.demo.rpc.utils;
 
+import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -7,19 +8,17 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 @Slf4j
 public class NetworkUtils {
 
-    private NetworkUtils() {
-    }
-
-    private static void bindPort(String host, int port) throws IOException {
-        try (Socket socket = new Socket()) {
-            log.debug("bind {}:{}", host, port);
-            socket.bind(new InetSocketAddress(host, port));
-        }
-    }
-
+    /**
+     * 端口是否可用
+     *
+     * @param port
+     * @return
+     */
     public static boolean isPortAvailable(int port) {
         try {
             bindPort("0.0.0.0", port);
@@ -27,6 +26,28 @@ public class NetworkUtils {
             return true;
         } catch (IOException e) {
             return false;
+        }
+    }
+
+    /**
+     * 端口是否被占用
+     *
+     * @param port
+     * @return
+     */
+    public static boolean isPortUnavailable(int port) {
+        return !isPortAvailable(port);
+    }
+
+    private NetworkUtils() {
+    }
+
+    private static void bindPort(String host, int port) throws IOException {
+        checkArgument((port >= 0 && port <= 65535), "port: %s (expected: 0-65535)", port);
+        checkArgument(!Strings.isNullOrEmpty(host), "host: %s is empty", host);
+        try (Socket socket = new Socket()) {
+            log.debug("bind {}:{}", host, port);
+            socket.bind(new InetSocketAddress(host, port));
         }
     }
 }
