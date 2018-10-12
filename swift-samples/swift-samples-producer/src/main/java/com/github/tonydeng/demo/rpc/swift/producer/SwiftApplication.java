604a4bcb338716e426f37fe36e847ca1af0a88b4
Tony Deng <wolf.deng@gmail.com>
2018-10-12 12:36:00 +0800

    :art: style(1.0): 清除一些不需要的代码
    refs #16
    
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- swift-samples/swift-samples-producer/src/main/java/com/github/tonydeng/demo/rpc/swift/producer/SwiftApplication.java
+++ swift-samples/swift-samples-producer/src/main/java/com/github/tonydeng/demo/rpc/swift/producer/SwiftApplication.java
@@ -7,23 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"com.github.tonydeng.demo.rpc.swift.producer"})
 public class SwiftApplication {
-    private static final int PORT = 9000;
 
     public static void main(String[] args) {
         SpringApplication.run(SwiftApplication.class, args);
-//        try {
-//            log.info("Thrift RPC's Server Start......");
-//
-//            TProcessor processor = new TBaseProcessor<UserService>(new UserServiceProducer());
-//            TServerSocket serverTransport = new TServerSocket(PORT);
-//
-//            TServer.Args tArgs = new TServer.Args(serverTransport);
-//            tArgs.processor(processor);
-//            tArgs.protocolFactory(new TBinaryProtocol.Factory());
-//            TServer server = new TSimpleServer(tArgs);
-//            server.serve();
-//        } catch (TTransportException e) {
-//            log.error("Thrift RPC's Server serve error \n", e);
-//        }
     }
 }
